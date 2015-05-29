7451d8e04515ac763d911d679572b0461b7c9195
BoomGitRepo <Rahul@rahulmac.cybage.com>
2015-05-29 21:25:17 +0530

    iOS Corona Example added
    Corona example of integrating Boom Library

---
+++ BoomCoronaDemoApp/AppCoronaDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppCoronaDelegate.h
+//  TemplateApp
+//
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "CoronaDelegate.h"
+#import "BoomVideoTrackerDelegate.h"
+
+@interface AppCoronaDelegate : NSObject< CoronaDelegate,BoomVideoTrackerDelegate >
+
+@end
